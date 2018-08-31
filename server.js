84d12a999964b3dce2b559f8e773c9aa20675ce0
fahimulhaque <ffahimulhaque9@gamil.com>
2018-08-31 09:54:45 +0530

    updated node server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/deployExample'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/dist/deployExample/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
